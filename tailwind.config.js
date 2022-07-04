deee530f31b4e9eba0fb09b661d6baa95a19551a
braveheart-star <mouse1996115@gmail.com>
2022-07-05 00:08:28 +0800

    implement validation on login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
   content: [
     "src/pages/**/*.{js,ts,jsx,tsx}",
     "src/components/**/*.{js,ts,jsx,tsx}",
+    "src/containers/**/*.{js,ts,jsx,tsx}",
   ],
   darkMode: "media", // or 'media' or 'class'
   theme: {
