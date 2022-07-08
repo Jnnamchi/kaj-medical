a05adfcc340de371b7de74aa2f482f7dd29d0632
braveheart-star <mouse1996115@gmail.com>
2022-07-09 05:10:14 +0800

    go back to home when done

--- src/containers/SurveyPage/index.tsx
+++ src/containers/SurveyPage/index.tsx
@@ -1,160 +1,14 @@
 import { useState } from "react";
 import { Form, Formik } from "formik";
 import * as Yup from "yup";
-import { userTypeOptions } from "../../utils/data";
+import { surveyFields } from "../../utils/data";
 import FormikControl from "../../components/surveyPage/SurveyFormikControl";
+import { useRouter } from "next/router";
 
 const SurveyPage = () => {
   const [step, setStep] = useState(0);
 
-  const fields = {
-    inquiry: [
-      {
-        name: "firstName",
-        label: "First Name",
-        control: "input",
-      },
-      {
-        name: "lastName",
-        label: "Last Name",
-        control: "input",
-      },
-
-      {
-        name: "email",
-        label: "Email (No gmail or major@mails) ",
-        control: "input",
-        type: "email",
-      },
-
-      {
-        name: "companySite",
-        label: "Company Website",
-        control: "input",
-      },
-
-      {
-        name: "companyEntity",
-        label: "Company or Entity Name",
-        control: "input",
-      },
-
-      {
-        name: "location",
-        label: "Location Registered",
-        control: "select",
-        options: userTypeOptions,
-      },
-
-      {
-        name: "entityType",
-        label: "Type of Entity",
-        control: "select",
-        options: userTypeOptions,
-      },
-      {
-        name: "requestDescription",
-        label: "Description of Request",
-        control: "textarea",
-      },
-    ],
-    document: {
-      licensed: [
-        {
-          name: "companyRegistration",
-          label: "Company Registration",
-          control: "input",
-          type: "file",
-        },
-        {
-          name: "licenseRegistration",
-          label: "License Registration documents / certifications",
-          control: "input",
-          type: "file",
-        },
-        {
-          name: "governmentRegistration",
-          label: "Government Registration Forms",
-          control: "input",
-          type: "file",
-        },
-      ],
-      manufacturer: [
-        {
-          name: "productCatalogue",
-          label: "Product Catalogue",
-          control: "input",
-          type: "file",
-        },
-        {
-          name: "certification",
-          label: "Certification(s)",
-          control: "input",
-          type: "file",
-        },
-      ],
-      brokerageAgency: [
-        {
-          name: "companyRegistration",
-          label: "Company Registration",
-          control: "input",
-          type: "file",
-        },
-        {
-          name: "registrationCertifications",
-          label: "Registration Certifications",
-          control: "input",
-          type: "file",
-        },
-        {
-          name: "VATnumberCode",
-          label: "Company VAT number / code",
-          control: "input",
-          type: "file",
-        },
-      ],
-      medicalFacility: [
-        {
-          name: "companyRegistration",
-          label: "Company Registration",
-          control: "input",
-          type: "file",
-        },
-      ],
-    },
-    ekyc: [
-      {
-        name: "firstName",
-        label: "First Name",
-        control: "input",
-      },
-      {
-        name: "lastName",
-        label: "Last Name",
-        control: "input",
-      },
-      {
-        name: "birth",
-        label: "Date of Birth",
-        control: "input",
-        type: "date",
-      },
-
-      {
-        name: "governmentId",
-        label: "Unique ID Number (Government ID or Passport Number)",
-        control: "input",
-      },
-
-      {
-        name: "passport",
-        label: "Passport or ID Copy",
-        control: "input",
-      },
-    ],
-  };
-
-  type ObjectKey = keyof typeof fields.document;
+  type ObjectKey = keyof typeof surveyFields.document;
 
   const validationSchema = Yup.object().shape({
     firstName: Yup.string().required("First Name Required"),
@@ -169,6 +23,7 @@ const SurveyPage = () => {
     ),
     // birth: Yup.string().required("Date of Birth Required"),
   });
+  const router = useRouter();
   return (
     <div className="max-w-2xl mx-auto ">
       <Formik
@@ -184,7 +39,9 @@ const SurveyPage = () => {
         }}
         validationSchema={validationSchema}
         onSubmit={() => {
-          if (step !== 2) {
+          if (step === 3) {
+            router.push("/");
+          } else {
             setStep(step + 1);
           }
         }}
@@ -194,24 +51,31 @@ const SurveyPage = () => {
             <Form>
               <div className="p-6 py-8 space-y-8">
                 {step === 0 && (
-                  <SurveyPart fields={fields.inquiry} formik={formik} />
+                  <SurveyPart fields={surveyFields.inquiry} formik={formik} />
                 )}
                 {step === 1 && (
                   <SurveyPart
                     fields={
-                      fields.document[formik.values.entityType as ObjectKey]
+                      surveyFields.document[
+                        formik.values.entityType as ObjectKey
+                      ]
                     }
                     formik={formik}
                   />
                 )}
-                {step === 2 && <SurveyPart fields={fields.ekyc} />}
+                {step === 2 && <SurveyPart fields={surveyFields.ekyc} />}
+                {step === 3 && <div>You are all set !</div>}
 
                 <div className="flex">
                   <button
                     type="submit"
                     className="px-4 py-2 m-auto border border-gray-600 rounded"
                   >
-                    {step === 2 ? "Finish" : "Continue"}
+                    {step === 0 || step === 1
+                      ? "Continue"
+                      : step === 2
+                      ? "Finish"
+                      : "Go To Home Page"}
                   </button>
                 </div>
               </div>
--- src/utils/data.ts
+++ src/utils/data.ts
@@ -35,3 +35,150 @@ export const documentPerEntity = {
     { value: "companyRegistration", label: "Company Registration" },
   ],
 };
+
+export const surveyFields = {
+  inquiry: [
+    {
+      name: "firstName",
+      label: "First Name",
+      control: "input",
+    },
+    {
+      name: "lastName",
+      label: "Last Name",
+      control: "input",
+    },
+
+    {
+      name: "email",
+      label: "Email (No gmail or major@mails) ",
+      control: "input",
+      type: "email",
+    },
+
+    {
+      name: "companySite",
+      label: "Company Website",
+      control: "input",
+    },
+
+    {
+      name: "companyEntity",
+      label: "Company or Entity Name",
+      control: "input",
+    },
+
+    {
+      name: "location",
+      label: "Location Registered",
+      control: "select",
+      options: userTypeOptions,
+    },
+
+    {
+      name: "entityType",
+      label: "Type of Entity",
+      control: "select",
+      options: userTypeOptions,
+    },
+    {
+      name: "requestDescription",
+      label: "Description of Request",
+      control: "textarea",
+    },
+  ],
+  document: {
+    licensed: [
+      {
+        name: "companyRegistration",
+        label: "Company Registration",
+        control: "input",
+        type: "file",
+      },
+      {
+        name: "licenseRegistration",
+        label: "License Registration documents / certifications",
+        control: "input",
+        type: "file",
+      },
+      {
+        name: "governmentRegistration",
+        label: "Government Registration Forms",
+        control: "input",
+        type: "file",
+      },
+    ],
+    manufacturer: [
+      {
+        name: "productCatalogue",
+        label: "Product Catalogue",
+        control: "input",
+        type: "file",
+      },
+      {
+        name: "certification",
+        label: "Certification(s)",
+        control: "input",
+        type: "file",
+      },
+    ],
+    brokerageAgency: [
+      {
+        name: "companyRegistration",
+        label: "Company Registration",
+        control: "input",
+        type: "file",
+      },
+      {
+        name: "registrationCertifications",
+        label: "Registration Certifications",
+        control: "input",
+        type: "file",
+      },
+      {
+        name: "VATnumberCode",
+        label: "Company VAT number / code",
+        control: "input",
+        type: "file",
+      },
+    ],
+    medicalFacility: [
+      {
+        name: "companyRegistration",
+        label: "Company Registration",
+        control: "input",
+        type: "file",
+      },
+    ],
+  },
+  ekyc: [
+    {
+      name: "firstName",
+      label: "First Name",
+      control: "input",
+    },
+    {
+      name: "lastName",
+      label: "Last Name",
+      control: "input",
+    },
+    {
+      name: "birth",
+      label: "Date of Birth",
+      control: "input",
+      type: "date",
+    },
+
+    {
+      name: "governmentId",
+      label: "Unique ID Number (Government ID or Passport Number)",
+      control: "input",
+    },
+
+    {
+      name: "passport",
+      label: "Passport or ID Copy",
+      control: "input",
+    },
+  ],
+};
