d2aa49b9a65326f08283a23991d7eb77801991ba
braveheart-star <mouse1996115@gmail.com>
2022-12-05 04:28:50 +0800

    test again

--- src/pages/api/tamper-detect.ts
+++ src/pages/api/tamper-detect.ts
@@ -3,8 +3,6 @@ import { NextApiRequest, NextApiResponse, PageConfig } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    console.log("🚀 ~ file: tamper-detect.ts:5 ~ handler ~ req", req);
-
     req.pipe(request("http://pdf-analyser.edpsciences.org/check")).pipe(res);
   } catch (e) {
     console.log("🚀 ~ file: tamper.ts:48 ~ handler ~ e", e);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,42 @@
+import axios from "axios";
+import { useEffect } from "react";
 import Layout from "../components/layout";
 import HomePage from "../containers/HomePage";
 
 export default function Home() {
+  useEffect(() => {
+    const getData = async () => {
+      const form: any = document.querySelector("form");
+
+      if (form) {
+        console.log("🚀 ~ file: index.tsx:11 ~ useEffect ~ form", form);
+        form.addEventListener("submit", async (e: any) => {
+          e.preventDefault();
+          const formData = new FormData(form);
+          await axios
+            .post("http://pdf-analyser.edpsciences.org/check", formData, {
+              headers: {
+                "Content-Type": "multipart/form-data"
+              }
+            })
+            .then((res) => {
+              console.log(res);
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        });
+      }
+    };
+    getData();
+  }, []);
+
   return (
     <Layout>
+      <form action="#" method="post">
+        <input type="file" name="doc" />
+        <button type="submit">Submit</button>
+      </form>
       <HomePage />
     </Layout>
   );
